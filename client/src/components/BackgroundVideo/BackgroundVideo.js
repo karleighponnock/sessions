3d2bc28850b36cf70840a3f6c065eca8d6178694
Jody Eggleston <jmeggles@gmail.com>
2020-08-17 16:40:47 -0400

    fixed variable and iframe property

--- client/src/components/BackgroundVideo/BackgroundVideo.js
+++ client/src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
 import classes from './BackgroundVideo.module.css';
 
-const BackgroundVideo = () => {
-    const videoSource = "file:///Users/karleighponnock/Downloads/production%20ID_4820525.mp4"
+const BackgroundVideo  = () => { "file:///Users/karleighponnock/Downloads/production%20ID_4820525.mp4"
     return (
         <div className={classes.Container} >
-          <iframe src="https://embed.wave.video/5f3ac9ed46e0fb000dae39cc" style={{left: "800px"}} height="1500px" width="1500px" frameborder="0" allow="loop; fullscreen"></iframe>
+          <iframe title="main page video" src="https://embed.wave.video/5f3ac9ed46e0fb000dae39cc" style={{left: "800px"}} height="1500px" width="1500px" frameborder="0" allow="loop; fullscreen"></iframe>
                 </div>
     )
 }
