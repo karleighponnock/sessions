20d48c5f38e8bd8e9ea6745386d058445c4ec1e8
Jody Eggleston <jmeggles@gmail.com>
2020-08-17 17:08:04 -0400

    fixed video home page to be responsive

--- client/src/components/BackgroundVideo/BackgroundVideo.js
+++ client/src/components/BackgroundVideo/BackgroundVideo.js
@@ -4,7 +4,7 @@ import classes from './BackgroundVideo.module.css';
 const BackgroundVideo  = () => { "file:///Users/karleighponnock/Downloads/production%20ID_4820525.mp4"
     return (
         <div className={classes.Container} >
-          <iframe title="main page video" src="https://embed.wave.video/5f3ac9ed46e0fb000dae39cc" style={{left: "800px"}} height="1500px" width="1500px" frameborder="0" allow="loop; fullscreen"></iframe>
+          <iframe title="main page video" src="https://embed.wave.video/5f3ac9ed46e0fb000dae39cc" style={{left: "800px"}} height="1500px" width="100%" frameborder="0" allow="loop; fullscreen"></iframe>
                 </div>
     )
 }
