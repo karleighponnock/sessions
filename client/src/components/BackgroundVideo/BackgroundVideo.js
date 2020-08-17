a976c2964adefb01f2ce7b33adfa8872abd5c47a
karleighponnock <karleighponnock@gmail.com>
2020-08-17 14:53:35 -0400

    video in place only needs to loop

--- client/src/components/BackgroundVideo/BackgroundVideo.js
+++ client/src/components/BackgroundVideo/BackgroundVideo.js
@@ -3,14 +3,10 @@ import React from 'react';
 import classes from './BackgroundVideo.module.css';
 
 const BackgroundVideo = () => {
-    const videoSource = "https://www.w3schools.com/tags/movie.mp4"
+    const videoSource = "file:///Users/karleighponnock/Downloads/production%20ID_4820525.mp4"
     return (
         <div className={classes.Container} >
-            <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
-                <source src={videoSource} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-
+          <iframe src="https://embed.wave.video/5f3ac9ed46e0fb000dae39cc" style={{left: "800px"}} height="1500px" width="1500px" frameborder="0" allow="loop; fullscreen"></iframe>
                 </div>
     )
 }
