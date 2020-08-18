b7b79fc3a931b19c24f4d3e445f55767ed21ceac
Jody Eggleston <jmeggles@gmail.com>
2020-08-18 11:50:36 -0400

    fixed uploaded image to show on page

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -2,7 +2,7 @@ const Category = require('../models/categoryModel')
 
 exports.createCategory = (req, res) => {
     let name = req.body.name
-    let image = req.file.path
+    let image = req.file.filename
     console.log(name, image)
     const category = new Category({
         name: name,
