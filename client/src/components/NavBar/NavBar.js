740dfed00fe314d1f4b7796e5a083e164ddde165
karleighponnock <karleighponnock@gmail.com>
2020-08-21 12:11:19 -0400

    navbar styling

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -13,15 +13,15 @@ export const NavBar = () => {
         if (isAuthenticated) {
             return (
                 <>
-                    <Link to="/dashboard" className="item">{currentUser.email}</Link>
-                    <Logout />
+                    <Link to="/dashboard" className="link">{currentUser.email}</Link>
+                    <Logout/>
                 </>
             )
         } else {
             return (
                 <>
-                    <Link to="/login" className="item">Login</Link>
-                    <Link to="/register" className="item">Register</Link>
+                    <Link to="/login" className=" link">Login</Link>
+                    <Link to="/register" className="link">Register</Link>
                 </>
             )
         }
@@ -30,9 +30,9 @@ export const NavBar = () => {
 
     return (
         <div className="ui inverted menu">
-            <Link to="/" className="item">Home</Link>
-            {isAuthenticated ? <Link to="/newsesh" className="item">New Session</Link> : ""}
-            {isAuthenticated ? <Link to="/mysesh" className="item">My Sessions</Link> : ""}
+            <Link to="/" className="link">Home</Link>
+            {isAuthenticated ? <Link to="/newsesh" className="link">New Session</Link> : ""}
+            {isAuthenticated ? <Link to="/mysesh" className="link">My Sessions</Link> : ""}
             <div className="right menu">
                 {showLinks()}
             </div>
