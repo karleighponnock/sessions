bffe97a9f77bf709e7118f43b56750f732c12bce
karleighponnock <karleighponnock@gmail.com>
2020-08-21 15:14:36 -0400

    avatar image area and upload button added

---
+++ client/src/components/Avatar/Avatar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import "./style.css"
+
+export function Avatar() {
+
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = e => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  return (
+    <div className="bio-pic">
+      <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        style={{
+          display: "none"
+        }}
+      />
+      <div
+        style={{
+          height: "360px",
+          width: "250px",
+          padding: "10px"
+        }}
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "relative"
+          }}
+        />
+      </div>
+      Click to upload Image
+    </div>
+
+
+
+   </div>
+  );
+}
+
+export default Avatar 
